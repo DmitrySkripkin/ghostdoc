fb65afe2bdd7f9518410336dd12f5293b33283f5
Unknown <sepia13@yandex.ru>
2018-05-21 00:32:46 +0300

    Some initial methods

--- index.js
+++ index.js
@@ -10,7 +10,69 @@ class GhostDoc {
     return this._content
   }
   set content (newContent) {
-    this._content = newContent
+    if (newContent) {
+      this._content = newContent
+    } else {
+      this._content = []
+    }
+  }
+
+  get amp () {
+    return this._amp || 1
+  }
+
+  set amp (value) {
+    this._amp = value
+  }
+
+  get length () {
+    return this.content.filter((symbol) => { return !symbol.deleted })
+  }
+
+  get plainText () {
+    if (this.content.length === 0) return ''
+    return this.content.reduce((acc, curr) => {
+      if (curr.deleted) return acc
+      return acc + curr.insert
+    }, '')
+  }
+
+  applyOperation (operation) {
+    let position = this.findPosition(operation.position)
+    this.amp = this.amp + 1
+    if (operation.insert) {
+      this.content.splice(position, 0, operation)
+    } else {
+      this.content[position].deleted = true
+    }
+  }
+
+  buildIndex () {
+    let i = 0
+    this._index = this.content.reduce((acc, curr) => {
+      acc[curr.hash] = i
+      i++
+      return acc
+    }, {})
+  }
+
+  get index () {
+    return this._index
+  }
+
+  _getPartForPositionSearch (hash) {
+    return this.content.slice(this.index[hash], Math.max(this.amp + 1, this.content.length))
+  }
+
+  findPosition (hash) {
+    let i = this.amp
+    for (let symbol of this._getPartForPositionSearch()) {
+      if (symbol.hash === hash) {
+        return i - 1
+      }
+      i++
+    }
+    return null
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -32,3 +32,72 @@ test('sets an empty content', t => {
   t.true(ghostDoc.content instanceof Array)
   t.true(ghostDoc.content.length === 0)
 })
+
+test('sets null content', t => {
+  const { ghostDoc } = t.context
+  ghostDoc.content = null
+  t.true(ghostDoc.content instanceof Array)
+  t.true(ghostDoc.content.length === 0)
+})
+
+test('sets a content', t => {
+  const { ghostDoc } = t.context
+  ghostDoc.content = [{ hash: '123', insert: 'a' }]
+  t.true(ghostDoc.content instanceof Array)
+  t.true(ghostDoc.content.length === 1)
+  t.true(ghostDoc.plainText === 'a')
+})
+
+test('gets an empty plaintext', t => {
+  const { ghostDoc } = t.context
+  t.true(ghostDoc.content instanceof Array)
+  t.true(ghostDoc.plainText === '')
+})
+
+test('_getPartForPositionSearch test', t => {
+  const { ghostDoc } = t.context
+  ghostDoc.content = [{ hash: '123', insert: 'a' }, { hash: '321', insert: 'b' }]
+  ghostDoc.buildIndex()
+  t.true(true)
+})
+
+test('get amp', t => {
+  const { ghostDoc } = t.context
+  t.true(ghostDoc.amp === 1)
+})
+
+test('set amp', t => {
+  const { ghostDoc } = t.context
+  ghostDoc.amp = 5
+  t.true(ghostDoc.amp === 5)
+})
+
+test('finds a position', t => {
+  const { ghostDoc } = t.context
+  ghostDoc.content = [{ hash: '123', insert: 'a' }, { hash: '321', insert: 'b' }, { hash: '222', insert: 'c' }]
+  ghostDoc.buildIndex()
+  t.true(ghostDoc.findPosition('222') === 2)
+})
+
+test('can\'t find a position', t => {
+  const { ghostDoc } = t.context
+  ghostDoc.content = [{ hash: '123', insert: 'a' }, { hash: '321', insert: 'b' }, { hash: '222', insert: 'c' }]
+  ghostDoc.buildIndex()
+  t.true(ghostDoc.findPosition('nope') === null)
+})
+
+test('applyes insert operation', t => {
+  const { ghostDoc } = t.context
+  ghostDoc.content = [{ hash: '123', insert: 'a' }, { hash: '321', insert: 'b' }, { hash: '222', insert: 'c' }]
+  ghostDoc.buildIndex()
+  ghostDoc.applyOperation({ hash: '444', position: '123', insert: 'a' })
+  t.true(ghostDoc.plainText === 'aabc')
+})
+
+test('applyes delete operation', t => {
+  const { ghostDoc } = t.context
+  ghostDoc.content = [{ hash: '123', insert: 'a' }, { hash: '321', insert: 'b' }, { hash: '222', insert: 'c' }]
+  ghostDoc.buildIndex()
+  ghostDoc.applyOperation({ position: '123' })
+  t.true(ghostDoc.plainText === 'bc')
+})
